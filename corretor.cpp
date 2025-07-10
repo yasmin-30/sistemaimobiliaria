5a247fdf0ecc124bdc90013c5ed561ca20df225f
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-07-10 12:03:23 -0300

    Update corretor.cpp

--- corretor.cpp
+++ corretor.cpp
@@ -6,3 +6,10 @@ Corretor::Corretor(std::string telefone, bool avaliador, double lat, double lon,
     : telefone(telefone), avaliador(avaliador), lat(lat), lon(lon), nome(nome) {
     id = proximoId++;
 }
+
+int Corretor::getId() const { return id; }
+std::string Corretor::getTelefone() const { return telefone; }
+bool Corretor::isAvaliador() const { return avaliador; }
+double Corretor::getLat() const { return lat; }
+double Corretor::getLon() const { return lon; }
+std::string Corretor::getNome() const { return nome; }
