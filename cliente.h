6ef8fadc37ea6cb4e29bbac66338ed3af1fa6492
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-07-10 12:16:25 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -5,12 +5,17 @@
 
 class Cliente {
 public:
+    int getId() const;
+    std::string getTelefone() const;
+    std::string getNome() const;
+
+    Cliente(std::string telefone, std::string nome);
+
+private:
     static int proximoId;
     int id;
     std::string telefone;
     std::string nome;
-
-    Cliente(std::string telefone, std::string nome);
 };
 
 #endif
