f844e43fbc4c6ff5445a829f7ee24d6f49570d31
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-07-10 12:17:20 -0300

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -2,6 +2,11 @@
 
 int Cliente::proximoId = 1;
 
-Cliente::Cliente(std::string telefone, std::string nome): telefone(telefone), nome(nome) {
+Cliente::Cliente(std::string telefone, std::string nome)
+    : telefone(telefone), nome(nome) {
     id = proximoId++;
 }
+
+int Cliente::getId() const { return id; }
+std::string Cliente::getTelefone() const { return telefone; }
+std::string Cliente::getNome() const { return nome; }
