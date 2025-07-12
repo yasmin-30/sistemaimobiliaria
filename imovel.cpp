d35e1fbd0d459bd3b587297fe7738b1e92f6f5c1
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-07-12 18:55:02 -0300

    Update imovel.cpp

--- imovel.cpp
+++ imovel.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include "imovel.h"
 
 using namespace std;
@@ -9,36 +8,52 @@ using namespace std;
 int Imovel::nextId = 1;
 
 //construtor parametrizado
-Imovel::Imovel(Tipo tipo, int proprietarioId, double lat, double lng, string endereco, double preco): id(nextId++), tipo(tipo), proprietarioId(proprietarioId), lat(lat), lng(lng), endereco(endereco), preco(preco) {}
+Imovel::Imovel(std::string tipo, int proprietarioId, double lat, double lng, double preco, string endereco)
+    : proprietarioId(proprietarioId), lat(lat), lng(lng), preco(preco), endereco(endereco) {
+    setTipo(tipo);
+    this->id = nextId++;
+}
 
 //destrutor
 Imovel::~Imovel() {}
 
 //métodos getter 
-int Imovel::getId() {
+int Imovel::getId() const {
     return this->id;
 }
 
-Tipo Imovel::getTipo() {
+Tipo Imovel::getTipo() const {
     return this->tipo;
 }
 
-int Imovel::getProprietarioId() {
+int Imovel::getProprietarioId() const {
     return this->proprietarioId;
 }
 
-double Imovel::getLat() {
+double Imovel::getLat() const {
     return this->lat;
 }
 
-double Imovel::getLng() {
+double Imovel::getLng() const {
     return this->lng;
 }
 
-string Imovel::getEndereco() {
+string Imovel::getEndereco() const {
     return this->endereco;
 }
 
-double Imovel::getPreco() {
+double Imovel::getPreco() const {
     return this->preco;
 }
+
+// Método setter
+void Imovel::setTipo(string tipo_str) {
+    if(tipo_str == "Casa") {
+        this->tipo = Casa;
+    } else if (tipo_str == "Apartamento") {
+        this->tipo = Apartamento;
+    } else if (tipo_str == "Terreno") {
+        this->tipo = Terreno;
+    }
+}
+
