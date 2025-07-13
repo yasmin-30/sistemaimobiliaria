374b9febd15d0b32915c77c129a584c36bff2cc4
TheoCamposSoares <161765940+TheoCamposSoares@users.noreply.github.com>
2025-07-13 12:35:56 -0300

    Delete agenda.h

--- agenda.h
+++
@@ -1,9 +0,0 @@
-#ifndef AGENDA_H
-#define AGENDA_H
-
-#include "gerenciamento.h"
-
-double haversine(double lat1, double lon1, double lat2, double lon2);
-void gerarAgenda(const Gerenciamento& g);
-
-#endif
